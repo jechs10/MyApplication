9f1d4cebbd30744286e030f880f2869593a75172
jechs10 <jc4007283@gmail.com>
2026-07-03 03:56:30 +0300

    Реализовано приложение со списком фильмов

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,73 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        analyzeIntList(listOf(67, -14, 22, -8, 15))
-        checkPasswordStrength("someTestPassword")
         setContent {
             MyApplicationTheme {
-                GuessNumberScreen()
+                var currentScreen by remember { mutableStateOf(0) }
+
+                if (currentScreen == 0) {
+                    FirstScreen(onNavigateToSecond = { currentScreen = 1 })
+                } else {
+                    SecondScreen()
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
-    }
-}
-fun analyzeIntList(input: List<Int>) {
-    val minimo = input.min()
-    val maximo = input.max()
-    val suma = input.sum()
-    val pares = input.count { it % 2 == 0 }
-    val impares = input.count { it % 2 != 0 }
-
-    println("Минимальное: $minimo")
-    println("Максимальное: $maximo")
-    println("Сумма: $suma")
-    println("Четных: $pares")
-    println("Нечетных: $impares")
-}
-
-fun checkPasswordStrength(password: String) {
-    var condiciones = 0
-
-    if (password.length >= 8) condiciones++
-    if (password.any { it.isDigit() }) condiciones++
-    if (password.any { it.isUpperCase() }) condiciones++
-    if (password.any { it.isLowerCase() }) condiciones++
-    if (password.any { !it.isLetterOrDigit() }) condiciones++
-
-    val resultado = when {
-        condiciones == 5 -> "надежный"
-        condiciones == 4 -> "хороший"
-        condiciones in 2..3 -> "средний"
-        else -> "ненадежный"
-    }
-
-    println("Надежность пароля: $resultado")
 }
